2a6202f7c7870a94ce5675274e6a898af5c12816
Erjankydyrov <Erjankydyrov29@gmail.com>
2021-02-12 13:57:05 +0600

    added mousedown

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 let div = document.querySelector("div");
 let canMove = false;
 
+div.addEventListener("mousedown", function(event) {
+   canMove = true;
+});
+
+ 
 document.body.addEventListener("mousemove", function(event) {
     if(canMove == true){
     div.style.left = event.clientX - 30 + "px";
